2363edc551628ad3509b0af9375053a9544eef91
fr33d <franklin.figueroa.27@unsch.edu.pe>
2024-05-30 16:03:19 -0500

    Auth Permisions into Devolucion

--- BBlizAPI/biblioteca_api/Views/devolucion_view.py
+++ BBlizAPI/biblioteca_api/Views/devolucion_view.py
@@ -2,8 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from ..Models.devolucion_model import Devolucion
 
 from ..Serializer.devolucion_serializer import DevolucionModelSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class DevolucionModelViewSet(ModelViewSet):
     serializer_class = DevolucionModelSerializer
     queryset = Devolucion.objects.all()
     http_method_names = ['post']
+    permission_classes = [IsAuthenticated]
